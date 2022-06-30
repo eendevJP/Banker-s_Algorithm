19636151e527bb922f92cd2c16830d33fa0b7ba5
João Pedro <55493281+eendevJP@users.noreply.github.com>
2022-06-30 13:51:45 -0300

    0.0.1 change
    Code Uptade

--- banqueiroEstatico.c
+++ banqueiroEstatico.c
@@ -7,16 +7,16 @@ int main(){
 	numProcessos = 5;
 	numRecursos = 3;
 	int allocation[5][3] = {{0, 1, 0}, //Processo 0 
-							{2, 0, 0}, //Processo 1 
-							{3, 0, 2}, //Processo 2 
-							{2, 1, 1}, //Processo 3 
-							{0, 0, 2}}; //Processo 4
+				{2, 0, 0}, //Processo 1 
+				{3, 0, 2}, //Processo 2 
+				{2, 1, 1}, //Processo 3 
+				{0, 0, 2}}; //Processo 4
 							
 	int max[5][3] = {{7, 5, 3}, //Necessario para P0 
-					{3, 2, 2}, //Necessario para P1
-					{9, 0, 2}, //Necessario para P2 
-					{2, 2, 2}, //Necessario para P3
-					{4, 3, 3}}; //Necessario para P4					
+			{3, 2, 2}, //Necessario para P1
+			{9, 0, 2}, //Necessario para P2 
+			{2, 2, 2}, //Necessario para P3
+			{4, 3, 3}}; //Necessario para P4					
 							
 	int disponivel[3] = {3, 3, 2};
 	
@@ -52,7 +52,7 @@ int main(){
 		}
 	}
 	
-	printf("A sequência segura eh\n");
+	printf("A sequÃªncia segura eh\n");
 	for(i = 0; i < numProcessos - 1; i++)
 		printf(" Processo%d ->", ans[i]);
 	printf(" Processo%d", ans[numProcessos - 1]);
